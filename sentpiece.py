9bc683fc8958ca0b73c7cdb645336ca316aaf82c
pavle <markovic.pavle@outlook.com>
2021-07-27 15:28:26 +0200

    Segmentation part.

---
+++ sentpiece.py
@@ -0,0 +1,23 @@
+from os import read
+import sentencepiece as spm
+
+
+def train_model(data_path, model_prefix, voc_size):
+    spm.SentencePieceTrainer.train(input=data_path, 
+                                   model_prefix=model_prefix,   
+                                   vocab_size=voc_size, 
+                                   character_coverage=1.0, 
+                                   model_type="bpe")
+
+
+def segmentation(data_path, model_prefix, out_file):
+    sp = spm.SentencePieceProcessor(model_file=f"models/{model_prefix}.model")
+
+    with open(data_path, 'r') as reader:
+        corpus = reader.read()
+
+    segmented = sp.encode(corpus, out_type=str)
+
+    with open(f"segmented_corpora/{out_file}", 'w') as writer:
+        writer.write(" ".join(segmented))
+
--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 import argparse
 import nltk
+from nltk import parse
 
 from preprocessor import Preprocessor
+from sentpiece import train_model, segmentation
 
 
 if __name__ == "__main__":
     nltk.download("punkt")
 
     parser = argparse.ArgumentParser(description="Test file!")
-    parser.add_argument("-corpath", default="data/alice_in_wonderland.txt", type=str, help="Path to corpus directory.")
+    parser.add_argument("-corpath", default="data/eng_train.txt", type=str, help="Path to corpus directory.")
     parser.add_argument("-lang", default="eng", type=str, help="Language of the corpus.")
+    parser.add_argument("-m_prefix", default="pmodel", help="Prefix of the model.")
+    parser.add_argument("-voc_size", default=1000, help="Size of model vocabulary.")
+    parser.add_argument("-seg_file", default="en_s1.txt", type=str, help="File for segmented text.")
     args = parser.parse_args()
 
-    preprocessor = Preprocessor(args.corpath, args.lang)
-    cleaned_corpus = preprocessor.process()
-    preprocessor.split(cleaned_corpus)
+    #preprocessor = Preprocessor(args.corpath, args.lang)
+    #cleaned_corpus = preprocessor.process()
+    #preprocessor.split(cleaned_corpus)
+
+    #train_model(args.corpath, args.m_prefix, args.voc_size)
+    segmentation(args.corpath, args.m_prefix, args.seg_file)
+
+
