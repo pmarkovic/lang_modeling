ccb7c6a1f3d9446a7ab85ced7a97c4bab64834ba
pavle <markovic.pavle@outlook.com>
2021-07-28 11:16:05 +0200

    Refactoring dirs

--- train_script.sh
+++ train_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-rm model
-rm model.output.txt
+rm rnnlm/models/model
+rm rnnlm/models/model.output.txt
 
-#rnn model is trained here
-time ../rnnlm -train $1 -valid $2 -rnnlm model -hidden $3 -rand-seed 1 -debug 2 -class 9999 -bptt $4
+time ./rnnlm/rnnlm -train $1 -valid $2 -rnnlm model -hidden $3 -rand-seed 1 -debug 2 -class 9999 -bptt $4
