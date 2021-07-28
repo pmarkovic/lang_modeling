2fd1052f7dea66c4cabb46b9db39d2daa24b11bd
pavle <markovic.pavle@outlook.com>
2021-07-28 11:13:48 +0200

    Data generation script

---
+++ gen_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+for i in {1,2,3,4,5,6,7}
+do
+    len=$((10 ** $i))
+    file_path="$1$len.txt"
+    
+    ./rnnlm/rnnlm -rnnlm ./rnnlm/models/model -gen $len -debug 0 > $file_path
+done
